18fcbc93760ae681fa06f5055a0b3ed8ff97fdec
joshua-hashimoto <dropscar041205@gmail.com>
2022-09-28 01:37:59 +0900

    fix: spectre.cssをbeercssに置き換え

--- static/js/modal.js
+++
@@ -1,9 +0,0 @@
-const elements = document.querySelectorAll(".show-modal, .close-modal");
-
-for (const element of elements) {
-    element.addEventListener("click", (event) => {
-        const modalId = element.dataset.target;
-        const modal = document.getElementById(modalId);
-        modal.classList.toggle("active");
-    });
-}
